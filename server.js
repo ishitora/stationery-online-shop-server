1dc2eea8b87053faa047bddf1dd6b4ea77cf81c3
yowang <ishitora00@gmail.com>
2021-07-27 14:30:17 +0800

    取消推送config 加入dev環境變數

--- server.js
+++ server.js
@@ -2,17 +2,17 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = require('./app');
 
-dotenv.config({ path: './config.env' });
+dotenv.config({ path: './config/dev.env' });
 
 console.log(process.env.NODE_ENV);
 
 mongoose
-  .connect('mongodb://localhost:27017/stationeryshop', {
+  .connect(process.env.DATABASE, {
     useNewUrlParser: true,
   })
   .then(() => {
-    app.listen('5000', () => {
-      console.log('連結成功  http://localhost:5000');
+    app.listen(process.env.PORT, () => {
+      console.log(`連結成功  http://localhost:${process.env.PORT}`);
     });
   })
   .catch((error) => {
